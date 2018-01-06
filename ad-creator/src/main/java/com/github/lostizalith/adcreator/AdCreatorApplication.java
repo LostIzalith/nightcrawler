55509440cf06c865c68976d709b2692fc5eab589
karpukdm <offspace@yandex.ru>
2018-01-06 21:12:59 +0300

    create tool skeleton

---
+++ ad-creator/src/main/java/com/github/lostizalith/adcreator/AdCreatorApplication.java
@@ -0,0 +1,12 @@
+package com.github.lostizalith.adcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdCreatorApplication.class, args);
+	}
+}
