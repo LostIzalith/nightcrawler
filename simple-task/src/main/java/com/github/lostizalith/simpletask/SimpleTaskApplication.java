6dda37156066d12bdd3ead060b057860dff62e5b
solairerove <hax2033@gmail.com>
2018-01-06 19:18:37 +0300

    feature: create simple spring boot application

---
+++ simple-task/src/main/java/com/github/lostizalith/simpletask/SimpleTaskApplication.java
@@ -0,0 +1,14 @@
+package com.github.lostizalith.simpletask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Simple spring boot application.
+ */
+@SpringBootApplication
+public class SimpleTaskApplication {
+    public static void main(final String... args) {
+        SpringApplication.run(SimpleTaskApplication.class, args);
+    }
+}
