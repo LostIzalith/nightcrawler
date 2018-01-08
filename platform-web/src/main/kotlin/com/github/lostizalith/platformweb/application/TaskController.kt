263115ea79e358e203a3815e98d6fb53d7b1b4fe
solairerove <hax2033@gmail.com>
2018-01-09 00:19:50 +0300

    feature: fix code style imports

--- platform-web/src/main/kotlin/com/github/lostizalith/platformweb/application/TaskController.kt
+++ platform-web/src/main/kotlin/com/github/lostizalith/platformweb/application/TaskController.kt
@@ -3,7 +3,11 @@ package com.github.lostizalith.platformweb.application
 import org.dozer.DozerBeanMapper
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * Rest endpoint to handle task execution.
