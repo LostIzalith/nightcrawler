6fe7cb2eb0f5a7258612acb37613abc61ef65a6c
solairerove <hax2033@gmail.com>
2018-01-10 00:07:23 +0300

    feature: stub prototype platform web implementation

--- platform-web/src/main/kotlin/com/github/lostizalith/platformweb/domain/TaskConfigurationHandler.kt
+++ platform-web/src/main/kotlin/com/github/lostizalith/platformweb/domain/TaskConfigurationHandler.kt
@@ -1,15 +1,36 @@
 package com.github.lostizalith.platformweb.domain
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
 
 @Service
-class TaskConfigurationHandler : TaskConfigurationHandling {
+class TaskConfigurationHandler(
+        @Autowired private val applicationContext: ApplicationContext
+) : TaskConfigurationHandling {
 
     override fun handle(appName: String, taskConfiguration: GreetingTaskConfiguration): TaskResult {
         // TODO: not implemented yet.
-        // TODO: producer and consumer
-        // TODO: result manager
+        // TODO: some additional info due configManagement
 
-        return TaskResult("string id", "CREATED", taskConfiguration, "SOME RESULT")
+        // TODO: result manager stub like we register(save) task
+        val taskResult = TaskResult("string id", "CREATED", taskConfiguration, "SOME RESULT")
+
+        val task = Task(taskResult.id, taskResult.configuration)
+
+        // TODO: producer and consumer
+        // TODO: maybe it will be cool to use RabbitMQ as queue
+
+        // TODO: it's like we produce and consume some task
+        // TODO: then we execute it ¯\_(ツ)_/¯
+        val taskExecutor = applicationContext.getBean(task.configuration.executorClass())
+        val result = taskExecutor.execute(task)
+
+        // TODO: then update in db WTF
+        taskResult.id = result.id
+        taskResult.status = "LIKE IN PROGRESS"
+        taskResult.result = result.data
+
+        return taskResult
     }
 }
